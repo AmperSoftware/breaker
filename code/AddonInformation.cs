e76d93deee9921b868ee097068979f7ac0a6f546
Kilian <noqualitycont@gmail.com>
2023-03-26 14:52:35 +0200

    Add chat logger for default chat box element

--- code/AddonInformation.cs
+++ code/AddonInformation.cs
@@ -12,5 +12,6 @@ namespace Breaker
 		public const string ADDON_VERSION = "0.1.3";
 		public const string BRANCH = "master";
 		public const string PREFIX = "[Breaker]";
+		public const string ICON = "https://cdn.discordapp.com/icons/834153880115085363/af4a52605f67eebab9d70884191a92e9.webp?size=64";
 	}
 }
