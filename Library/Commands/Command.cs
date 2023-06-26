79a38072e5a5f5fc2e7201bc30424ff0528ae154
Kilian <noqualitycont@gmail.com>
2023-06-26 16:21:12 +0200

    Merge master into menu

--- Library/Commands/Command.cs
+++ Library/Commands/Command.cs
@@ -13,224 +13,228 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Breaker
+namespace Breaker;
+
+public static partial class Command
 {
-	public static partial class Command
+	public struct ContextInfo
 	{
-		public struct ContextInfo
+		public IClient Caller;
+		public float CallTick;
+		public DateTime CallTime;
+
+		public ContextInfo( IClient caller )
 		{
-			public IClient Caller;
-			public float CallTick;
-			public DateTime CallTime;
-
-			public ContextInfo( IClient caller )
-			{
-				Caller = caller;
-				CallTick = Time.Tick;
-				CallTime = DateTime.Now;
-			}
-		}
-		public class Info
-		{
-			public string Name = "";
-			public string Group = "";
-			public CommandAttribute Attribute;
-			public MethodDescription Method;
-
-			public Info( CommandAttribute attribute, MethodDescription method )
-			{
-				Attribute = attribute;
-				Method = method;
-			}
-
-			public ParameterInfo[] GetParameters() => Method.Parameters;
-		}
-		public class ClientInfo
-		{
-			public string Key;
-			public string Name;
-			public string Group;
-			public string[] Parameters;
-
-			public ClientInfo( string key, string name, string group = "", params string[] parameters )
-			{
-				Key = key;
-				Name = name;
-				Group = group;
-				Parameters = parameters;
-			}
-
-			public static implicit operator ClientInfo( Info info )
-			{
-				return new ClientInfo( info.Attribute.Key, info.Name, info.Group, info.GetParameters().Select(p => p.Name).ToArray() );
-			}
-		}
-		private static Dictionary<string, Info> commands = new();
-		private static Dictionary<string, Info> aliasToCommand = new();
-
-		private static List<ClientInfo> clientCommands = new();
-		/// <summary>
-		/// All current commands. Only usable on the server.
-		/// </summary>
-		public static IReadOnlyDictionary<string, Info> All => commands.AsReadOnly();
-		public static ContextInfo Context { get; private set; }
-		public static IClient Caller => Context.Caller;
-		public static string CallerName => Caller?.Name ?? "Console";
-		/// <summary>
-		/// Deletes the current list of commands and generates a new one.
-		/// </summary>
-		[BRKEvent.ConfigLoaded]
-		public static void LoadAll()
-		{
-			commands.Clear();
-			aliasToCommand.Clear();
-			NetworkCommandClear();
-
-			var types = TypeLibrary.GetTypes();
-			foreach ( var type in types )
-			{
-				foreach ( var method in type.Methods )
-				{
-					if ( method.IsStatic && method.IsMethod )
-						LoadMethod( method, type );
-				}
-			}
-		}
-
-		private static void LoadMethod(MethodDescription method, TypeDescription type)
-		{
-			var attribute = method.GetCustomAttribute<CommandAttribute>();
-
-			if ( attribute == null )
-			{
-				var builtinAttrib = method.GetCustomAttribute<ConCmd.AdminAttribute>();
-				if ( builtinAttrib == null )
-					return;
-
-				string attributeName = builtinAttrib.Name ?? method.Name.ToLower();
-				attribute = new CommandAttribute( attributeName ) { Description = builtinAttrib.Help, GeneratedFrom = type.Namespace };
-			}
-
-			var name = attribute.Key;
-
-			// Get group from method or type
-			string group = null;
-
-			if ( !string.IsNullOrWhiteSpace( method.Group ) )
-				group = method.Group;
-			else if ( !string.IsNullOrWhiteSpace( type.Group ) )
-				group = type.Group;
-
-			string title = name;
-			if ( !string.IsNullOrWhiteSpace(method.Title) )
-			{
-				title = method.Title;
-			}
-
-			Info info = new( attribute, method ) { Name = title, Group = group };
-			if ( commands.ContainsKey( name ) )
-			{
-				Logging.Error( $"Tried to register command with duplicate name {name}!" );
-				return;
-			}
-
-			foreach ( var alias in attribute.Aliases )
-			{
-				if ( commands.ContainsKey( alias ) )
-				{
-					Logging.Error( $"Tried to register command with alias {alias} which already exists as a command!" );
-				}
-				else if ( aliasToCommand.ContainsKey( alias ) )
-				{
-					Logging.Error( $"Tried to register command alias {alias} which already exists!" );
-					continue;
-				}
-
-				Debug.Log( $"Alias for {name}: {alias}" );
-				aliasToCommand.Add( alias, info );
-			}
-
-			Debug.Log( $"Registering command {name}." );
-			commands.Add( name, info );
-			NetworkCommandInfo( name, title, group, method.Parameters.Select( p => p.Name ).ToArray() );
-		}
-
-		#region Networking
-		[GameEvent.Server.ClientJoined]
-		static void OnClientJoin( ClientJoinedEvent ev )
-		{
-			var client = ev.Client;
-			foreach(var kv in commands)
-			{
-				var cmd = kv.Value;
-				NetworkCommandInfo( To.Single( client ), kv.Key, cmd.Name, cmd.Group, cmd.GetParameters().Select(p => p.Name).ToArray() );
-			}
-			
-		}
-		[ClientRpc]
-		public static void NetworkCommandInfo(string key, string name, string group = "", string[] parameters = default )
-		{
-			ClientInfo info = new( key, name, group, parameters );
-			clientCommands.Add( info );
-		}
-		[ClientRpc]
-		public static void NetworkCommandClear()
-		{
-			clientCommands.Clear();
-		}
-		#endregion
-
-		public static ParameterInfo[] Parameters( string name )
-		{
-			if(!commands.TryGetValue(name, out var cmd))
-			{
-				Log.Error( $"Tried to get parameters of command {name} which does not exist!" );
-				return default;
-			}
-
-			return cmd.Method.Parameters;
-		}
-		public static PermissionAttribute[] Permissions(string name)
-		{
-			if ( !commands.TryGetValue( name, out var cmd ) )
-			{
-				Log.Error( $"Tried to get required permissions of command {name} which does not exist!" );
-				return default;
-			}
-
-			if ( cmd.Attribute.IsGenerated )
-			{
-				return new PermissionAttribute[] { new($"other.{cmd.Attribute.GeneratedFrom.ToLowerInvariant()}.{name}") };
-			}
-
-			return cmd.Method.Attributes.OfType<PermissionAttribute>().ToArray();
-		}
-
-		public static IEnumerable<ClientInfo> GetAllClient()
-		{
-			return clientCommands;
-		}
-
-		public static IEnumerable<IGrouping<string, ClientInfo>> GetAllClientGrouped()
-		{
-			return clientCommands.GroupBy( info => info.Group );
+			Caller = caller;
+			CallTick = Time.Tick;
+			CallTime = DateTime.Now;
 		}
 	}
-	[AttributeUsage( AttributeTargets.Method )]
-	public class CommandAttribute : Attribute
+	public class Info
 	{
-		public string Key { get; set; }
-		public string[] Aliases { get; }
-		public string Description { get; set; }
-		internal string GeneratedFrom = "";
-		internal bool IsGenerated => !string.IsNullOrWhiteSpace( GeneratedFrom );
-		public CommandAttribute(string key, params string[] aliases)
-		{
-			if ( string.IsNullOrEmpty( key ) )
-				throw new ArgumentNullException( nameof(key) );
+		public string Name = "";
+		public string Group = "";
+		public CommandAttribute Attribute;
+		public MethodDescription Method;
 
-			Key = key;
-			Aliases = aliases;
+		public Info( CommandAttribute attribute, MethodDescription method )
+		{
+			Attribute = attribute;
+			Method = method;
 		}
+
+		public ParameterInfo[] GetParameters() => Method.Parameters;
+	}
+	public class ClientInfo
+	{
+		public string Key;
+		public string Name;
+		public string Group;
+		public string[] Parameters;
+
+		public ClientInfo( string key, string name, string group = "", params string[] parameters )
+		{
+			Key = key;
+			Name = name;
+			Group = group;
+			Parameters = parameters;
+		}
+
+		public static implicit operator ClientInfo( Info info )
+		{
+			return new ClientInfo( info.Attribute.Key, info.Name, info.Group, info.GetParameters().Select(p => p.Name).ToArray() );
+		}
+	}
+	private static Dictionary<string, Info> commands = new();
+	private static Dictionary<string, Info> aliasToCommand = new();
+
+	private static List<ClientInfo> clientCommands = new();
+	/// <summary>
+	/// All current commands. Only usable on the server.
+	/// </summary>
+	public static IReadOnlyDictionary<string, Info> All => commands.AsReadOnly();
+	public static ContextInfo Context { get; private set; }
+	public static IClient Caller => Context.Caller;
+	public static string CallerName => Caller?.Name ?? "Console";
+	/// <summary>
+	/// Deletes the current list of commands and generates a new one.
+	/// </summary>
+	[BRKEvent.ConfigLoaded]
+	public static void LoadAll()
+	{
+		commands.Clear();
+		aliasToCommand.Clear();
+		NetworkCommandClear();
+
+		var types = TypeLibrary.GetTypes();
+		foreach ( var type in types )
+		{
+			foreach ( var method in type.Methods )
+			{
+				if ( method.IsStatic && method.IsMethod )
+					LoadMethod( method, type );
+			}
+		}
+	}
+
+	private static void LoadMethod(MethodDescription method, TypeDescription type)
+	{
+		var attribute = method.GetCustomAttribute<CommandAttribute>();
+
+		if ( Console.Vars.breaker_autogen_builtin_commands && attribute == null )
+		{
+			var builtinAttrib = method.GetCustomAttribute<ConCmd.AdminAttribute>();
+			if(builtinAttrib != null)
+			{
+				string attributeName = builtinAttrib.Name ?? method.Name.ToLowerInvariant();
+				string source = type.Namespace?.ToLowerInvariant() ?? type.Name.ToLowerInvariant();
+				attribute = new CommandAttribute( attributeName ) { Description = builtinAttrib.Help, GeneratedFrom = source };
+				Log.Info( attribute.IsGenerated );
+			}
+		}
+
+		if ( attribute == null )
+			return;
+
+		var name = attribute.Key;
+
+		// Get group from method or type
+		string group = null;
+
+		if ( !string.IsNullOrWhiteSpace( method.Group ) )
+			group = method.Group;
+		else if ( !string.IsNullOrWhiteSpace( type.Group ) )
+			group = type.Group;
+
+		string title = name;
+		if ( !string.IsNullOrWhiteSpace(method.Title) )
+		{
+			title = method.Title;
+		}
+
+		Info info = new( attribute, method ) { Name = title, Group = group };
+		if ( commands.ContainsKey( name ) )
+		{
+			Logging.Error( $"Tried to register command with duplicate name {name}!" );
+			return;
+		}
+
+		foreach ( var alias in attribute.Aliases )
+		{
+			if ( commands.ContainsKey( alias ) )
+			{
+				Logging.Error( $"Tried to register command with alias {alias} which already exists as a command!" );
+			}
+			else if ( aliasToCommand.ContainsKey( alias ) )
+			{
+				Logging.Error( $"Tried to register command alias {alias} which already exists!" );
+				continue;
+			}
+
+			Debug.Log( $"Alias for {name}: {alias}" );
+			aliasToCommand.Add( alias, info );
+		}
+
+		Debug.Log( $"Registering command {name}." );
+		commands.Add( name, info );
+		NetworkCommandInfo( name, title, group, method.Parameters.Select( p => p.Name ).ToArray() );
+	}
+
+	#region Networking
+	[GameEvent.Server.ClientJoined]
+	static void OnClientJoin( ClientJoinedEvent ev )
+	{
+		var client = ev.Client;
+		foreach(var kv in commands)
+		{
+			var cmd = kv.Value;
+			NetworkCommandInfo( To.Single( client ), kv.Key, cmd.Name, cmd.Group, cmd.GetParameters().Select(p => p.Name).ToArray() );
+		}
+		
+	}
+	[ClientRpc]
+	public static void NetworkCommandInfo(string key, string name, string group = "", string[] parameters = default )
+	{
+		ClientInfo info = new( key, name, group, parameters );
+		clientCommands.Add( info );
+	}
+	[ClientRpc]
+	public static void NetworkCommandClear()
+	{
+		clientCommands.Clear();
+	}
+	#endregion
+
+	public static ParameterInfo[] Parameters( string name )
+	{
+		if(!commands.TryGetValue(name, out var cmd))
+		{
+			Log.Error( $"Tried to get parameters of command {name} which does not exist!" );
+			return default;
+		}
+
+		return cmd.Method.Parameters;
+	}
+	public static PermissionAttribute[] Permissions(string name)
+	{
+		if ( !commands.TryGetValue( name, out var cmd ) )
+		{
+			Log.Error( $"Tried to get required permissions of command {name} which does not exist!" );
+			return default;
+		}
+
+		if ( cmd.Attribute.IsGenerated )
+		{
+			return new PermissionAttribute[] { new($"other.{cmd.Attribute.GeneratedFrom}.{name}") };
+		}
+
+		return cmd.Method.Attributes.OfType<PermissionAttribute>().ToArray();
+	}
+
+	public static IEnumerable<ClientInfo> GetAllClient()
+	{
+		return clientCommands;
+	}
+
+	public static IEnumerable<IGrouping<string, ClientInfo>> GetAllClientGrouped()
+	{
+		return clientCommands.GroupBy( info => info.Group );
+	}
+}
+[AttributeUsage( AttributeTargets.Method )]
+public class CommandAttribute : Attribute
+{
+	public string Key { get; set; }
+	public string[] Aliases { get; }
+	public string Description { get; set; }
+	internal string GeneratedFrom = "";
+	internal bool IsGenerated => !string.IsNullOrEmpty( GeneratedFrom );
+	public CommandAttribute(string key, params string[] aliases)
+	{
+		if ( string.IsNullOrEmpty( key ) )
+			throw new ArgumentNullException( nameof(key) );
+
+		Key = key;
+		Aliases = aliases;
 	}
 }
--- Library/Console.cs
+++ Library/Console.cs
@@ -7,139 +7,139 @@ using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Breaker
+namespace Breaker;
+
+[Category( "Basic" )]
+public static class Console
 {
-	[Category( "Basic" )]
-	public static class Console
+	/// <summary>
+	/// All the basic breaker commands.
+	/// </summary>
+	public static class Commands
 	{
-		/// <summary>
-		/// All the basic breaker commands.
-		/// </summary>
-		public static class Commands
+		#region S&box commands
+		[ConCmd.Server( "brk" )]
+		public static void RunCommand( string command, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" )
 		{
-			#region S&box commands
-			[ConCmd.Server( "brk" )]
-			public static void RunCommand( string command, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" )
+			if ( !Vars.breaker_enabled )
 			{
-				if ( !Vars.breaker_enabled )
-				{
-					Logging.TellClient( ConsoleSystem.Caller, "Breaker is disabled!", MessageType.Error );
-					return;
-				}
-
-				var parameters = new string[] { p1, p2, p3, p4, p5, p6 };
-				Command.Execute( command, ConsoleSystem.Caller, parameters.Where( p => !string.IsNullOrEmpty( p ) ).ToArray() );
-			}
-			// Theres no way to define aliases for ConCmds, so we have to do this.
-			[ConCmd.Server( "bkr" )] public static void RunCommandAlias1( string cmd, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" ) => RunCommand( cmd, p1, p2, p3, p4, p5, p6 );
-			[ConCmd.Server( "breaker" )] public static void RunCommandAlias2( string cmd, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" ) => RunCommand( cmd, p1, p2, p3, p4, p5, p6 );
-			#endregion S&box commands
-
-			[Command( "announce" ), Permission( "breaker.announce" )]
-			public static void Announce( string msg )
-			{
-				Logging.TellAll( msg, MessageType.Announcement );
-			}
-			[Command( "reload" ), Permission( "breaker.reload" )]
-			public static void Reload()
-			{
-				Logging.TellCaller( "Reloading..." );
-				Config.Load();
-
-				Logging.TellCaller( "Finished Reloading!" );
-				Logging.TellCaller( $"{Command.All.Count} commands loaded" );
-				Logging.TellCaller( $"{User.All.Count} users loaded" );
-				Logging.TellCaller( $"{UserGroup.All.Count} user groups loaded" );
+				Logging.TellClient( ConsoleSystem.Caller, "Breaker is disabled!", MessageType.Error );
+				return;
 			}
 
-			const int COMMANDS_PER_PAGE = 10;
-			[Command( "help", "?" )]
-			public static void Help( int page = 1 )
+			var parameters = new string[] { p1, p2, p3, p4, p5, p6 };
+			Command.Execute( command, ConsoleSystem.Caller, parameters.Where( p => !string.IsNullOrEmpty( p ) ).ToArray() );
+		}
+		// Theres no way to define aliases for ConCmds, so we have to do this.
+		[ConCmd.Server( "bkr" )] public static void RunCommandAlias1( string cmd, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" ) => RunCommand( cmd, p1, p2, p3, p4, p5, p6 );
+		[ConCmd.Server( "breaker" )] public static void RunCommandAlias2( string cmd, string p1 = "", string p2 = "", string p3 = "", string p4 = "", string p5 = "", string p6 = "" ) => RunCommand( cmd, p1, p2, p3, p4, p5, p6 );
+		#endregion S&box commands
+
+		[Command( "announce" ), Permission( "breaker.announce" )]
+		public static void Announce( string msg )
+		{
+			Logging.TellAll( msg, MessageType.Announcement );
+		}
+		[Command( "reload" ), Permission( "breaker.reload" )]
+		public static void Reload()
+		{
+			Logging.TellCaller( "Reloading..." );
+			Config.Load();
+
+			Logging.TellCaller( "Finished Reloading!" );
+			Logging.TellCaller( $"{Command.All.Count} commands loaded" );
+			Logging.TellCaller( $"{User.All.Count} users loaded" );
+			Logging.TellCaller( $"{UserGroup.All.Count} user groups loaded" );
+		}
+
+		const int COMMANDS_PER_PAGE = 10;
+		[Command( "help", "?" )]
+		public static void Help( int page = 1 )
+		{
+			if ( page < 1 )
+				page = 1;
+
+			var cmds = Command.All.OrderBy( kv => kv.Key ).ToArray();
+			int length = cmds.Length;
+			int pageCount = MathX.CeilToInt( length / (float)COMMANDS_PER_PAGE );
+			if ( pageCount < 1 )
+				pageCount = 1;
+
+			int pageStart = COMMANDS_PER_PAGE * (page - 1);
+			if ( pageStart >= length )
 			{
-				if ( page < 1 )
-					page = 1;
-
-				var cmds = Command.All.OrderBy( kv => kv.Key ).ToArray();
-				int length = cmds.Length;
-				int pageCount = MathX.CeilToInt( length / (float)COMMANDS_PER_PAGE );
-				if ( pageCount < 1 )
-					pageCount = 1;
-
-				int pageStart = COMMANDS_PER_PAGE * (page - 1);
-				if ( pageStart >= length )
-				{
-					Logging.TellCaller( $"Page {page} does not exist." );
-					return;
-				}
-				int pageEnd = pageStart + COMMANDS_PER_PAGE;
-				if ( pageEnd > length )
-					pageEnd = length;
-
-				Logging.TellCaller( "============" );
-				Logging.TellCaller( $"{AddonInformation.ADDON_NAME} ({AddonInformation.ADDON_VERSION}/{AddonInformation.BRANCH})" );
-				Logging.TellCaller( "============" );
-
-				Logging.TellCaller( $"{length} commands currently registered." );
-				Logging.TellCaller( $"Page {page} of {pageCount}" );
-				for ( int i = pageStart; i < pageEnd; i++ )
-				{
-					PrintCommand( cmds[i] );
-				}
+				Logging.TellCaller( $"Page {page} does not exist." );
+				return;
 			}
+			int pageEnd = pageStart + COMMANDS_PER_PAGE;
+			if ( pageEnd > length )
+				pageEnd = length;
 
-			private static void PrintCommand( KeyValuePair<string, Command.Info> cmd )
-			{
-				string name = cmd.Key;
-				var p = Command.Parameters( name );
-				if ( p.Length > 0 )
-				{
-					name += $" ({string.Join( ", ", p.Select( x => $"{PrettyParamName( x )}: {PrettyTypeName( x.ParameterType )}" ) )})";
-				}
-				string desc = cmd.Value.Method.Description;
-				if ( !string.IsNullOrEmpty( desc ) )
-					name += $"| {desc}";
+			Logging.TellCaller( "============" );
+			Logging.TellCaller( $"{AddonInformation.ADDON_NAME} ({AddonInformation.ADDON_VERSION}/{AddonInformation.BRANCH})" );
+			Logging.TellCaller( "============" );
 
-				Logging.TellCaller( $"- {name}" );
-			}
-			private static string PrettyParamName( ParameterInfo p )
+			Logging.TellCaller( $"{length} commands currently registered." );
+			Logging.TellCaller( $"Page {page} of {pageCount}" );
+			for ( int i = pageStart; i < pageEnd; i++ )
 			{
-				var title = p.GetCustomAttribute<TitleAttribute>();
-				if ( title != null )
-					return title.Value;
-
-				return p.Name;
-			}
-			private static string PrettyTypeName( Type t )
-			{
-				if ( t == typeof( string ) )
-					return "String";
-				if ( t == typeof( int ) )
-					return "Integer";
-				if ( t == typeof( float ) )
-					return "Float";
-				if ( t == typeof( bool ) )
-					return "Boolean";
-				if ( t == typeof( long ) )
-					return "Long";
-				if ( t == typeof( Vector3 ) )
-					return "Vector";
-				if ( t == typeof( IClient ) )
-					return "Client";
-				if ( t == typeof( IClient[] ) )
-					return "Clients";
-				return t.Name;
+				PrintCommand( cmds[i] );
 			}
 		}
 
-		/// <summary>
-		/// All the basic breaker console variables.
-		/// </summary>
-		public static class Vars
+		private static void PrintCommand( KeyValuePair<string, Command.Info> cmd )
 		{
-			[ConVar.Replicated] public static bool breaker_enabled { get; set; } = true;
-			[ConVar.Replicated] public static int breaker_reserved_slots { get; set; } = 0;
-			[ConVar.Replicated] public static bool breaker_whitelist_enabled { get; set; } = false;
-			[ConVar.Replicated] public static bool breaker_debug { get; set; } = false;
+			string name = cmd.Key;
+			var p = Command.Parameters( name );
+			if ( p.Length > 0 )
+			{
+				name += $" ({string.Join( ", ", p.Select( x => $"{PrettyParamName( x )}: {PrettyTypeName( x.ParameterType )}" ) )})";
+			}
+			string desc = cmd.Value.Method.Description;
+			if ( !string.IsNullOrEmpty( desc ) )
+				name += $"| {desc}";
+
+			Logging.TellCaller( $"- {name}" );
 		}
+		private static string PrettyParamName( ParameterInfo p )
+		{
+			var title = p.GetCustomAttribute<TitleAttribute>();
+			if ( title != null )
+				return title.Value;
+
+			return p.Name;
+		}
+		private static string PrettyTypeName( Type t )
+		{
+			if ( t == typeof( string ) )
+				return "String";
+			if ( t == typeof( int ) )
+				return "Integer";
+			if ( t == typeof( float ) )
+				return "Float";
+			if ( t == typeof( bool ) )
+				return "Boolean";
+			if ( t == typeof( long ) )
+				return "Long";
+			if ( t == typeof( Vector3 ) )
+				return "Vector";
+			if ( t == typeof( IClient ) )
+				return "Client";
+			if ( t == typeof( IClient[] ) )
+				return "Clients";
+			return t.Name;
+		}
+	}
+
+	/// <summary>
+	/// All the basic breaker console variables.
+	/// </summary>
+	public static class Vars
+	{
+		[ConVar.Replicated] public static bool breaker_enabled { get; set; } = true;
+		[ConVar.Replicated] public static int breaker_reserved_slots { get; set; } = 0;
+		[ConVar.Replicated] public static bool breaker_whitelist_enabled { get; set; } = false;
+		[ConVar.Replicated] public static bool breaker_debug { get; set; } = false;
+		[ConVar.Replicated] public static bool breaker_autogen_builtin_commands { get; set; } = false;
 	}
 }
